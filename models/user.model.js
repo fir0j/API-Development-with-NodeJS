c6949f5ff44db8aa7e1d13a759f370fa035963c5
fir0j <firoj.is.availble@gmail.com>
2019-10-10 13:08:40 +0530

    demystified populate method

--- models/user.model.js
+++ models/user.model.js
@@ -13,3 +13,8 @@ const userSchema = new mongoose.Schema({
 });
 
 module.exports = mongoose.model('USER', userSchema);
+
+//By Default, mongoose uses plural form of name of the model and creates collection with that name if collection is specified.
+//So above line will refer to the collection name called users
+//however you can force mongoose to use your own name for the collection by below syntax
+// module.exports = mongoose.model('USER', userSchema, 'usersCollection');
