6f1b55d45e0af808dd373a7ab4d546182d8d5ede
fir0j <firoj.is.availble@gmail.com>
2019-10-27 01:02:50 +0530

    updated mongoose driver

--- server.js
+++ server.js
@@ -13,8 +13,20 @@ app.listen(port, () => {
 	console.log(`Server is running successfully on port: ${port}`);
 });
 
-//connecting the server to the database
+/*
+There are two drivers for connecting with mongodb database.
+1) mongodb driver:
+> It is a native driver to interact with mongodb which accepts mongodb shell command as it is.
+> It cannot create Models or schema or relationship between data like in sql.
+> It is faster than mongoose but if you use this you have to make your SOURCE CODE PUBLIC 
+  because it is managed under Affero General Public License (AGPL) license.
 
+2) mongoose driver:
+> It has ability to create data models or schema at implementation level called schema abstraction 
+  because the schema will not exist physically in the mongodb database.
+> It is managed under MIT liscence so you can host your code in public domain without making it oper source.
+
+*/
 mongoose
 	.connect(process.env.URL, { useNewUrlParser: true, useUnifiedTopology: true })
 	.then(() => console.log('Database connected'));
